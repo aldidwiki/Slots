95ee18e3fc56dc75ad6bb2378949d713535d36d5
Aldi Dwiki Prahasta <aldidwiki@gmail.com>
2022-04-10 10:57:28 +0700

    add layout and logic

--- Slots/ContentView.swift
+++ Slots/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var credits = 1000
+    @State private var first = "apple"
+    @State private var second = "star"
+    @State private var third = "cherry"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("SwiftUI Slots!")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .padding()
+            Spacer()
+            Text("Credits : \(String(credits))")
+            Spacer()
+            HStack {
+                Image(first).resizable().aspectRatio(contentMode: ContentMode.fit).padding(.horizontal)
+                Image(second).resizable().aspectRatio(contentMode: ContentMode.fit).padding(.horizontal)
+                Image(third).resizable().aspectRatio(contentMode: ContentMode.fit).padding(.horizontal)
+            }.padding(.horizontal)
+            Spacer()
+            
+            Button {
+                let myArray = ["star", "apple", "cherry"]
+                first = myArray.randomElement()!
+                second = myArray.randomElement()!
+                third = myArray.randomElement()!
+                
+                if first == second && first == third && second == third {
+                    credits += 10
+                } else {
+                    credits -= 5
+                }
+                
+            } label: {
+                Text("Spin")
+                    .font(.title2)
+                    .foregroundColor(Color.white)
+                    .padding(.horizontal, 36)
+                    .padding(.vertical, 6)
+            }
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.red/*@END_MENU_TOKEN@*/)
+            .cornerRadius(/*@START_MENU_TOKEN@*/16.0/*@END_MENU_TOKEN@*/)
+            Spacer()
+        }
     }
 }
 
