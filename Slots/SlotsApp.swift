f9c8e1610149b7ad29dcaad9806e7304ab459b9f
Aldi Dwiki Prahasta <aldidwiki@gmail.com>
2022-04-10 10:24:55 +0700

    Initial commit

---
+++ Slots/SlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotsApp.swift
+//  Slots
+//
+//  Created by Aldi Dwiki Prahasta on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
